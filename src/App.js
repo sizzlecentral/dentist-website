0758b83ebb004d686d2630b0dc5532ddbc98da47
Don Cooper <don_cooper@hotmail.com>
2017-09-05 13:38:50 -0400

    Procedures and Contact components created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Home from './Home';
+import Procedures from './Procedures';
+import Contact from './Contact';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Home></Home>
+        <Procedures></Procedures>
+        <Contact></Contact>
       </div>
     );
   }
 }
+
 export default App;
