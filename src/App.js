ddcd400f972fc2d9da716fcdd2ec720e913f1d39
Don Cooper <don_cooper@hotmail.com>
2017-09-05 13:34:06 -0400

    Home component created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Dentist Website</h1>
-        <p>
-          Welcome to my dentist website.
-        </p>
+        <Home></Home>
       </div>
     );
   }
 }
-
 export default App;
