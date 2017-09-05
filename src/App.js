28e68e9606bd9a5b19e8a717fc205ad95267dcd2
Don Cooper <don_cooper@hotmail.com>
2017-09-05 13:46:41 -0400

    Router implemented in project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Home from './Home';
 import Procedures from './Procedures';
 import Contact from './Contact';
 
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home></Home>
-        <Procedures></Procedures>
-        <Contact></Contact>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
